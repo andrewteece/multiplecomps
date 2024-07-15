304c35dc87172670bf43c0aed8b893a3de8e7dda
Andrew Teece <andrewteece@gmail.com>
2024-07-15 07:35:45 -0500

    update Card.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './Card';
 
 function App() {
   return (
     <div>
       <h1>Task: Add three Card elements</h1>
+      `<Card />` `<Card />` `<Card />`
     </div>
   );
 }
