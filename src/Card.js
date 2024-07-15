3b4ee4c0d8d13e5b60a3797565bfcb9df896f570
Andrew Teece <andrewteece@gmail.com>
2024-07-15 17:33:20 -0500

    update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,8 @@
 function Card(props) {
   return (
     <div className="card">
-      <h2>`{props.h2}`</h2>
-      <h3>`{props.h3}`</h3>
+      <h2>{props.h2}</h2>
+      <h3>{props.h3}</h3>
     </div>
   );
 }
